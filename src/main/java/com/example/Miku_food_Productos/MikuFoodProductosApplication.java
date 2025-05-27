7a588e5b689a6279e540df2a58af70042bfe69d2
EveningSk <Kevinbakanx12@gmail.com>
2025-05-27 17:32:37 -0400

    Version 2 funcionando

---
+++ src/main/java/com/example/Miku_food_Productos/MikuFoodProductosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Miku_food_Productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikuFoodProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikuFoodProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Miku_food_Productos/MikuFoodProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Miku_food_Productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikuFoodProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
